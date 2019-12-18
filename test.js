6569fc948977c9c31e16dc3ad083a6f9b815a617
yokoemon99 <58900342+yokoemon99@users.noreply.github.com>
2019-12-18 16:48:28 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('soundhandler', {
+    tick: function () {
+           var entity = document.querySelector('[sound]');
+         if (document.querySelector('a-marker').object3D.visible == true) {
+            entity.components.sound.playSound();
+        } else {
+            entity.components.sound.pauseSound();
+        }
+
+     }
+});
